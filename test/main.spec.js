2199bb35cfb7b9e1bca61df89972d1bc1ba71c54
Ömer Fatih <omer.f.tanriverdi@gmail.com>
2025-06-13 03:04:53 +0300

    Test performance improvements

--- test/main.spec.js
+++ test/main.spec.js
@@ -31,6 +31,7 @@ test("article sayısı doğru mu?", async ({ page }) => {
 
   const articles = await page.locator(".articleList > .article");
   await expect(articles).toHaveCount(newsData.length);
+  await page.close();
 });
 
 test("her article içinde gerekli elementler bulunuyor mu?", async ({ page }) => {
@@ -48,6 +49,7 @@ test("her article içinde gerekli elementler bulunuyor mu?", async ({ page }) =>
     await expect(article.locator("p")).toHaveCount(4); // tarih + 3 paragraf
     await expect(article.locator("button.expandButton")).toHaveCount(1);
   }
+  await page.close();
 });
 
 test("article başlık ve tarihleri doğru render ediliyor mu?", async ({ page }) => {
@@ -56,6 +58,7 @@ test("article başlık ve tarihleri doğru render ediliyor mu?", async ({ page }
   const firstArticle = page.locator(".articleList > .article").first();
   await expect(firstArticle.locator("h2")).toHaveText(newsData[0].baslik);
   await expect(firstArticle.locator("p.date")).toHaveText(newsData[0].tarih);
+  await page.close();
 });
 
 test("expandButton tıklanınca ilgili article'a isOpen classı ekleniyor mu?", async ({ page }) => {
@@ -66,6 +69,7 @@ test("expandButton tıklanınca ilgili article'a isOpen classı ekleniyor mu?", 
 
   await button.click();
   await expect(firstArticle).toHaveClass(/isOpen/);
+  await page.close();
 });
 
 test("bir article'da isOpen classı varken expandButton'a tıklanınca isOpen classı kaldırılıyor mu?", async ({ page }) => {
@@ -78,6 +82,7 @@ test("bir article'da isOpen classı varken expandButton'a tıklanınca isOpen cl
   await expect(firstArticle).toHaveClass(/isOpen/);
   await button.click();
   await expect(firstArticle).not.toHaveClass(/isOpen/);
+  await page.close();
 });
 
 test("expandButton textContent olarak '+' içeriyor mu?", async ({ page }) => {
@@ -90,6 +95,7 @@ test("expandButton textContent olarak '+' içeriyor mu?", async ({ page }) => {
   for (let i = 0; i < count; i++) {
     await expect(buttons.nth(i)).toHaveText("+");
   }
+  await page.close();
 });
 
 test("menu oluşturulmuş ve header içinde bulunuyor mu?", async ({ page }) => {
@@ -97,6 +103,7 @@ test("menu oluşturulmuş ve header içinde bulunuyor mu?", async ({ page }) => 
 
   const menu = page.locator(".header .menu");
   await expect(menu).toHaveCount(1);
+  await page.close();
 });
 
 test("menu içinde ul ve doğru sayıda li var mı?", async ({ page }) => {
@@ -107,6 +114,7 @@ test("menu içinde ul ve doğru sayıda li var mı?", async ({ page }) => {
 
   const liItems = page.locator(".menu ul li");
   await expect(liItems).toHaveCount(menuElemanlari.length);
+  await page.close();
 });
 
 test("li içerikleri menuElemanlari ile eşleşiyor mu?", async ({ page }) => {
@@ -117,6 +125,7 @@ test("li içerikleri menuElemanlari ile eşleşiyor mu?", async ({ page }) => {
   for (let i = 0; i < menuElemanlari.length; i++) {
     await expect(liItems.nth(i)).toHaveText(menuElemanlari[i]);
   }
+  await page.close();
 });
 
 test("menu-button'a tıklanınca isOpen classı ekleniyor mu?", async ({ page }) => {
@@ -127,6 +136,7 @@ test("menu-button'a tıklanınca isOpen classı ekleniyor mu?", async ({ page })
 
   await menuButton.click();
   await expect(menu).toHaveClass(/isOpen/);
+  await page.close();
 });
 
 test("menu-button'a tekrar tıklanınca isOpen classı kaldırılıyor mu?", async ({ page }) => {
@@ -139,6 +149,7 @@ test("menu-button'a tekrar tıklanınca isOpen classı kaldırılıyor mu?", asy
   await expect(menu).toHaveClass(/isOpen/);
   await menuButton.click();
   await expect(menu).not.toHaveClass(/isOpen/);
+  await page.close();
 });
 
 test("ilk açılışta menu isOpen classı içermiyor olmalı", async ({ page }) => {
@@ -146,6 +157,7 @@ test("ilk açılışta menu isOpen classı içermiyor olmalı", async ({ page })
 
   const menu = page.locator(".menu");
   await expect(menu).not.toHaveClass(/isOpen/);
+  await page.close();
 });
 
 test("index.html içeriği değişmemiş olmalı", async () => {
